63013427e0f831b942edd4754ad1c315809d35cb
joshdsolis <joshdsolis@gmail.com>
2019-05-15 11:01:13 -0500

    aws eb fiddling

--- application.py
+++ application.py
@@ -8,19 +8,20 @@ from predict import generate
 import os
 from flask_cors import CORS, cross_origin
 
+
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-app = Flask(__name__)
-CORS(app, resources=r"*")
+application = Flask(__name__)
+CORS(application, resources=r"*")
 
-app.config["DEBUG"] = True
+application.config["DEBUG"] = True
 
 
-@app.route('/', methods=['GET',"POST"])
+@application.route('/', methods=['GET',"POST"])
 def home():
     
     return render_template('home.html')
 
-@app.route('/generator/', methods = ['POST', 'OPTIONS'])
+@application.route('/generator/', methods = ['POST', 'OPTIONS'])
 @cross_origin(origin='*')
 def music_generator():
     
@@ -40,4 +41,4 @@ def music_generator():
     return send_file('test_output.mid', mimetype='audio/midi', as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+    application.run(debug=True, threaded=True)
