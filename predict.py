aabd170bce64aeae5b7dae4dc78f726529e95ebd
joshdsolis <joshdsolis@gmail.com>
2019-05-20 10:40:26 -0500

    added beethoven midi files

--- predict.py
+++ predict.py
@@ -62,7 +62,7 @@ def create_network(network_input, n_vocab, artist, style, mod):
     model.add(Dropout(0.3))
     model.add(LSTM(int(mod), return_sequences=True))
     model.add(Dropout(0.3))
-    model.add(LSTM(int(mod))
+    model.add(LSTM(int(mod)))
     model.add(Dense(256))
     model.add(Dropout(0.3))
     model.add(Dense(n_vocab))
